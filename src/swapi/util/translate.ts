de086f0cbbcaa07e9e5e4f9d6a1afab3639cbc60
Rosenberg Vasquez <rosenbergvr@hotmail.com>
2024-10-24 16:19:58 -0500

    change add replace text function

--- src/swapi/util/translate.ts
+++ src/swapi/util/translate.ts
@@ -13,11 +13,30 @@ export default class Translate {
   private async translateText(text: string) {
     return await translate(text, { to: "es" })
       .then((res) => {
-        return res.text.replaceAll(" ", "_");
+        return this.reemplazarTildes(res.text);
       })
       .catch((err) => {
         console.error(err);
         return "attribute_not_translate";
       });
+  }
+  private reemplazarTildes(texto: string): string {
+    const mapaTildes: { [key: string]: string } = {
+        'á': 'a',
+        'é': 'e',
+        'í': 'i',
+        'ó': 'o',
+        'ú': 'u',
+        'Á': 'A',
+        'É': 'E',
+        'Í': 'I',
+        'Ó': 'O',
+        'Ú': 'U',
+        'ñ': 'n',
+        'Ñ': 'N',
+        " ": "_",
+    };
+
+    return texto.replace(/[áéíóúÁÉÍÓÚñÑ]/g, (match) => mapaTildes[match]);
   }
 }
